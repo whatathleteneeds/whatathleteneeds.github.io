e4c3f0cbce00ebc5fdad18bb5969ca3932e7ba2f
Alaa Ali <alaasdk@gmail.com>
2024-05-06 17:46:44 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,9 +36,13 @@
 
 					// Images (in the format of 'url': 'alignment').
 						images: {
-							'images/bg01.jpg': 'center',
-							'images/bg02.jpg': 'center',
-							'images/bg03.jpg': 'center'
+							// 'images/bg01.jpg': 'center',
+							// 'images/bg02.jpg': 'center',
+							// 'images/bg03.jpg': 'center'
+
+							'images/bg04.jpg': 'center',
+							'images/bg05.jpg': 'center',
+							'images/bg07.jpg': 'center'
 						},
 
 					// Delay.
